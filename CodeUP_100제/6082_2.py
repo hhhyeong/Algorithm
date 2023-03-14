f43c27cd1ddac4a75f59afd9be2717ad7cfa76da
hhhyeong <yommyhy77@gmail.com>
2023-03-15 01:21:51 +0900

    23/03/14

--- CodeUP_100제/6082_2.py
+++ CodeUP_100제/6082_2.py
@@ -2,13 +2,13 @@ n = int(input())
 
 for i in range(1,n+1):
     # 일의자리에 3,6,9가 포함되어있는 경우
-    if (i<10) and i % 10 % 3 == 0:
+    if (i<10) and i % 10 == 3:
         print('X',end=' ')
     # 10의자리에 3이 포함되어있는 경우
-    elif i % 10 % 3 == 0 and (i / 10 % 3 != 0):
+    elif i % 10 == 3 and (i / 10 != 3):
         print('X',end=' ')
-    elif i % 10 % 3 == 0 and (i / 10 % 3 == 0):
-        print('XX',end=' ')    
+    elif i % 10 == 3 and (i / 10 == 3):
+        print('XX',end=' ')
     # print(" ")
     else:
         print(i, end=" ")
